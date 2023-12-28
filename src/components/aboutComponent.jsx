fa84b978fe9a42aeb743c3724676886a7371d09c
ctokoli <matumasie@gmail.com>
2023-12-28 18:09:35 +0000

    chore: add about component

---
+++ src/components/aboutComponent.jsx
@@ -0,0 +1,10 @@
+const AboutComponent = () => {
+    return ( 
+        <div className="about">
+            <h1>About</h1>
+            <p>About page body content</p>
+        </div>
+     );
+}
+ 
+export default AboutComponent;
