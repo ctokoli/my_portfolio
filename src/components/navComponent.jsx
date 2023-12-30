37734fbdea07b142a5b33989dac352fab8267e9d
ctokoli <matumasie@gmail.com>
2023-12-30 17:51:58 +0000

    Style: style  nav items

--- src/components/navComponent.jsx
+++ src/components/navComponent.jsx
@@ -1,7 +1,13 @@
 const NavComponent = () => {
     return ( 
-        <div>
-             <h1 className="text-orange-500">Side Bar</h1>
+        <div className="nav mt-20">
+            <ul className="[&>li]:p-10 [&>li]:text-xl [&>li]:tracking-wider [&>li]:font-light [&>li]:cursor-pointer">
+                <li className="hover:bg-slate-600 hover:bg-opacity-10"><a href="#">About</a></li>
+                <li className="hover:bg-slate-600 hover:bg-opacity-10"> <a href="#">Experiences</a></li>
+                <li className="hover:bg-slate-600 hover:bg-opacity-10"> <a href="#">Education</a></li>
+                <li className="hover:bg-slate-600 hover:bg-opacity-10"><a href="#">Projects</a></li>
+                <li className="hover:bg-slate-600 hover:bg-opacity-10"><a href="#">Contact</a></li>
+            </ul>
         </div>
      );
 }
