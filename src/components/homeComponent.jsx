f218f64e671e4e688d83020eaa7ba90b17cf496f
ctokoli <matumasie@gmail.com>
2024-01-03 16:25:47 +0000

    Update homeComponent.jsx

--- src/components/homeComponent.jsx
+++ src/components/homeComponent.jsx
@@ -1,12 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import SideBarComponent from "./sideBarComponent";
-import ContentComponent from "./contentComponet";
+import AboutComponent from './aboutComponent';
+import EducationComponent from './educationComponent';
+import ProjectComponent from './projectComponent';
+import ContactComponent from './contactComponent';
+import ExperienceComponent from './experienceComponent';
+
 
 const HomeComponent = () => {
     return ( 
-        <div className="home bg-[#0c1225] bg-opacity-80 md:flex">
-            <SideBarComponent />
-            <ContentComponent />
-        </div>
+        <Router>
+            <div className="home bg-[#0c1225] bg-opacity-80 md:flex">
+                <SideBarComponent />
+                <Routes>
+                    <Route path='/' element={<AboutComponent />} />
+                    <Route path='/education'  element={<EducationComponent />} />
+                    <Route path='/projects' element={<ProjectComponent />} />
+                    <Route path='/contact' element={< ContactComponent />} />
+                    <Route path='/experience' element={< ExperienceComponent />} />
+                </Routes>
+            </div>
+        </Router>
      );
 }
  
