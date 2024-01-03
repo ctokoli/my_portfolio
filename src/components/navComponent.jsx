471f02f71ba8f7cf4a5625fc7492bdfb51a2f9ad
ctokoli <matumasie@gmail.com>
2024-01-03 16:25:47 +0000

    Update navComponent.jsx

--- src/components/navComponent.jsx
+++ src/components/navComponent.jsx
@@ -2,11 +2,11 @@ const NavComponent = () => {
     return ( 
         <div className="nav mt-20">
             <ul className="[&>li]:p-8 [&>li]:text-xl [&>li]:tracking-wider [&>li]:font-light [&>li]:cursor-pointer">
-                <li className="hover:bg-slate-600 hover:bg-opacity-10"><a href="#">About</a></li>
-                <li className="hover:bg-slate-600 hover:bg-opacity-10"> <a href="#">Experiences</a></li>
-                <li className="hover:bg-slate-600 hover:bg-opacity-10"> <a href="#">Education</a></li>
-                <li className="hover:bg-slate-600 hover:bg-opacity-10"><a href="#">Projects</a></li>
-                <li className="hover:bg-slate-600 hover:bg-opacity-10"><a href="#">Contact</a></li>
+                <li className="hover:bg-slate-600 hover:bg-opacity-10"><a href="/">About</a></li>
+                <li className="hover:bg-slate-600 hover:bg-opacity-10"> <a href="/experience">Experiences</a></li>
+                <li className="hover:bg-slate-600 hover:bg-opacity-10"> <a href="/education">Education</a></li>
+                <li className="hover:bg-slate-600 hover:bg-opacity-10"><a href="/projects">Projects</a></li>
+                <li className="hover:bg-slate-600 hover:bg-opacity-10"><a href="/contact">Contact</a></li>
             </ul>
         </div>
      );
