c38843a4c94a4c6722db9ecb91441e77bd06018c
ctokoli <matumasie@gmail.com>
2024-01-06 21:58:00 +0000

    Update navComponent.jsx

--- src/components/navComponent.jsx
+++ src/components/navComponent.jsx
@@ -2,7 +2,7 @@ const NavComponent = () => {
     return ( 
         <div className="nav mt-20">
             <ul className="[&>li]:p-8 [&>li]:text-xl [&>li]:tracking-wider [&>li]:font-light [&>li]:cursor-pointer">
-                <li className="hover:bg-slate-600 hover:bg-opacity-10"><a href="/">About</a></li>
+                <li className="hover:bg-slate-600 hover:bg-opacity-10"><a href="/about">About</a></li>
                 <li className="hover:bg-slate-600 hover:bg-opacity-10"> <a href="/experience">Experiences</a></li>
                 <li className="hover:bg-slate-600 hover:bg-opacity-10"> <a href="/education">Education</a></li>
                 <li className="hover:bg-slate-600 hover:bg-opacity-10"><a href="/projects">Projects</a></li>
