238bedb9a48d08c7425a3858fb2da14d3e0db300
ctokoli <matumasie@gmail.com>
2024-01-06 21:58:00 +0000

    Update timelineComponent.jsx

--- src/components/timelineComponent.jsx
+++ src/components/timelineComponent.jsx
@@ -1,39 +1,75 @@
 const TimelineComponents = () => {
+      
     return ( 
-        <div>
-                <div className="max-w-2xl mx-auto">
-                {/* Timeline Container */}
-                    <div className="relative wrap overflow-hidden p-10 h-full">
-                        
-                        {/* Timeline Dots */}
-                        <div className="border-2-2 absolute border-opacity-20 border-gray-700 h-full border" style={{ left: '50%' }}></div>
+        <div className="mt-20 ml-20">
+    <div className="timeline">
+  <div className="timeline__event  animated fadeInUp delay-3s timeline__event--type1">
+    <div className="timeline__event__icon ">
+      <i className="lni-cake"></i>
+      <div className="timeline__event__date">
+        20-08-2019
+      </div>
+    </div>
+    <div className="timeline__event__content ">
+      <div className="timeline__event__title">
+        Birthday
+      </div>
+      <div className="timeline__event__description">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam! Nam eveniet ut aliquam ab asperiores, accusamus iure veniam corporis incidunt reprehenderit accusantium id aut architecto harum quidem dolorem in!</p>
+      </div>
+    </div>
+  </div>
+  <div className="timeline__event animated fadeInUp delay-2s timeline__event--type2">
+    <div className="timeline__event__icon">
+      <i className="lni-burger"></i>
+      <div className="timeline__event__date">
+        20-08-2019
+      </div>
+    </div>
+    <div className="timeline__event__content">
+      <div className="timeline__event__title">
+        Lunch
+      </div>
+      <div className="timeline__event__description">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam! Nam eveniet ut aliquam ab asperiores, accusamus iure veniam corporis incidunt reprehenderit accusantium id aut architecto harum quidem dolorem in!</p>
+      </div>
+    </div>
+  </div>
+  <div className="timeline__event animated fadeInUp delay-1s timeline__event--type3">
+    <div className="timeline__event__icon">
+      <i className="lni-slim"></i>
+      <div className="timeline__event__date">
+        20-08-2019
+      </div>
+    </div>
+    <div className="timeline__event__content">
+      <div className="timeline__event__title">
+        Exercise
+      </div>
+      <div className="timeline__event__description">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam! Nam eveniet ut aliquam ab asperiores, accusamus iure veniam corporis incidunt reprehenderit accusantium id aut architecto harum quidem dolorem in!</p>
+      </div>
 
-                        {/* Timeline Items */}
-                        <div className="mb-8 flex justify-between items-center w-full right-timeline">
-                            <div className="order-1 w-5/12"></div>
-                            <div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-                                <h1 className="mx-auto text-white font-semibold text-lg">1</h1>
-                            </div>
-                            <div className="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-                                <h3 className="mb-3 font-bold text-gray-800 text-xl">Event 1</h3>
-                                <p className="text-sm leading-snug tracking-wide text-gray-900">Description of event 1.</p>
-                                <p className="text-xs text-gray-600">Date: January 1, 2022</p>
-                            </div>
-                        </div>
+    </div>
+  </div>
+  <div className="timeline__event animated fadeInUp timeline__event--type1">
+    <div className="timeline__event__icon">
+      <i className="lni-cake"></i>
+      <div className="timeline__event__date">
+        20-08-2019
+      </div>
+    </div>
+    <div className="timeline__event__content">
+      <div className="timeline__event__title">
+        Birthday
+      </div>
+      <div className="timeline__event__description">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, nam! Nam eveniet ut aliquam ab asperiores, accusamus iure veniam corporis incidunt reprehenderit accusantium id aut architecto harum quidem dolorem in!</p>
+      </div>
+    </div>
+  </div>
 
-                        <div className="mb-8 flex justify-between items-center w-full right-timeline">
-                            <div className="order-1 w-5/12"></div>
-                            <div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-                                <h1 className="mx-auto text-white font-semibold text-lg">2</h1>
-                            </div>
-                            <div className="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-                                <h3 className="mb-3 font-bold text-gray-800 text-xl">Event 2</h3>
-                                <p className="text-sm leading-snug tracking-wide text-gray-900">Description of event 2.</p>
-                                <p className="text-xs text-gray-600">Date: February 15, 2022</p>
-                            </div>
-                    </div>
-                </div>
-            </div>
+</div>
         </div>
   );
 }
