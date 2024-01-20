9877177a844656afc160f02c22b5738bb3f61cc7
Cephas Tokoli <matumasie@gmail.com>
2024-01-20 02:04:44 +0000

    Update educationComponent.jsx

--- src/components/educationComponent.jsx
+++ src/components/educationComponent.jsx
@@ -1,9 +1,14 @@
+import EducationTimeline from "./educationTimeline";
+
 const EducationComponent = () => {
     return (
-        <>
-            <h1>Education</h1>
-        </>
-    )
+        <div className="exp p-24 ">
+            <div className=" w-36 ">
+            <h1 className="text-2xl   border-pink-500 border-b-2 ">Education</h1>
+            </div>
+            <EducationTimeline />
+        </div>
+)
 }
 
 export default EducationComponent;
