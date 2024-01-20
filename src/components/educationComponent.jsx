a266dfa74a995f981679293e72f075009853ca1b
Cephas Tokoli <matumasie@gmail.com>
2024-01-20 21:19:49 +0000

    Update educationComponent.jsx

--- src/components/educationComponent.jsx
+++ src/components/educationComponent.jsx
@@ -2,9 +2,9 @@ import EducationTimeline from "./educationTimeline";
 
 const EducationComponent = () => {
     return (
-        <div className="exp p-24 ">
+        <div className="exp md:p-24 ">
             <div className=" w-36 ">
-            <h1 className="text-2xl   border-pink-500 border-b-2 ">Education</h1>
+            <h1 className="text-2xl ex-h1   border-pink-500 border-b-2 ">Education</h1>
             </div>
             <EducationTimeline />
         </div>
