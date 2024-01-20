123e13be8e5782a190024cf669c4fe37104ae060
Cephas Tokoli <matumasie@gmail.com>
2024-01-20 18:48:06 +0000

    Update aboutComponent.jsx

--- src/components/aboutComponent.jsx
+++ src/components/aboutComponent.jsx
@@ -2,13 +2,13 @@ import StacksComponents from "./stacks";
 
 const AboutComponent = () => {
     return ( 
-        <div className=" w-8/12 md:p-24 sm:p-5">
+        <div className="about md:w-8/12 md:p-24 sm:p-10">
             <div className="w-20 mt-5">
                 <h1 className="text-2xl border-pink-500 border-b-2">About</h1>
             </div>
-            <h1 className=" text-6xl mt-5 ">Cephas Tokoli</h1>
+            <h1 className=" text-5xl  mt-5 ">Cephas Tokoli</h1>
             <h2 className="mt-5 text-3xl text-pink-500">Full-Stack Developer</h2>
-            <div className="bg-size max-w-24 mt-3 opacity-60 ">
+            <div className="bg-size md:max-w-24 mt-3 opacity-60 ">
                 <p className=" text-xl leading-9 "> I&apos;m a  𝗙𝘂𝗹𝗹-𝗦𝘁𝗮𝗰𝗸 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿 and Mentor with a experience in Frontend and Backend Development spanning 10 years in the IT industry. Proficient in JavaScript, React, Ruby on Rails, Flutter, and Node.js.  Throughout my  career, I&apos;ve transitioned through different roles and developed more than 50+ websites using WordPress, Reactjs and other tools.</p>
             </div>
              <div>
