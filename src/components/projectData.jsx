37ef5716cc3d0bae1a77e7630d2165f6f6d264d3
Cephas Tokoli <matumasie@gmail.com>
2024-01-22 16:20:55 +0000

    Update projectData.jsx

--- src/components/projectData.jsx
+++ src/components/projectData.jsx
@@ -2,7 +2,7 @@ const data = [
     {
         "project_name": 'Dragula',
         "description": 'Dragula is a React Single Page Application designed for file uploads, featuring both sign-in and sign-up functionality using Firebase Authentication.',
-        "project_image" : './src/assets/images/dragula.png',
+        "project_image" : './images/dragula.png',
         "link": "https://dragula.onrender.com",
         "github": "https://github.com/ctokoli/Dragula"
 
@@ -10,35 +10,35 @@ const data = [
     {
         "project_name": 'Spaceship',
         "description": 'Spaceship: You will have access to rockets, missions and dragons even to reserve and participate through our react application.',
-        "project_image" : './src/assets/images/spaceship.png',
+        "project_image" : './images/spaceship.png',
         "link": "https://rocket-missions.netlify.app/",
         "github": "https://github.com/rubydevi/react-group-project",
     },
     {
         "project_name": 'Franko Trading Ecommerce Mobile app',
         "description": 'Franko Trading Mobile App developed with #flutter and Woocommerce API with payment system, cart management, and an address management system.',
-        "project_image" : './src/assets/images/mobile_app.png',
+        "project_image" : './images/mobile_app.png',
         "link": "https://play.google.com/store/apps/details?id=com.frakotg.enterapp&hl=en&gl=US",
         "github": ""
     },
     {
         "project_name": 'Movie App',
         "description": 'This project was created using some JavaScript features like webpack, ES6 features, and Jest testing framework. We consume tvmaze API and UI styling.',
-        "project_image" : './src/assets/images/movie_app_1.png',
+        "project_image" : './images/movie_app_1.png',
         "link": "https://moviehouse.onrender.com",
         "github": "https://github.com/ctokoli/JavaScript-Group-Capstone-Project"
     },
     {
         "project_name": 'Movie App',
         "description": 'MovieBox is a React Single Page Application that shows top 10 rated movies from The Movie DB (TMDB) API.',
-        "project_image" : './src/assets/images/movie_app_2.png',
+        "project_image" : './images/movie_app_2.png',
         "link": "https://moviebox-a18k.onrender.com",
         "github": "https://github.com/ctokoli/Movie-Box"
     },
     {
         "project_name": 'Calculator',
         "description": 'This is a React project were i create a single page Application using React JS Technology. I create a Calculator App.',
-        "project_image" : './src/assets/images/math.png',
+        "project_image" : './images/math.png',
         "link": "https://react-calculator-a58z.onrender.com",
         "github": "https://github.com/ctokoli/math-magicians-react-app"
     }
