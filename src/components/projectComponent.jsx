0200254597cd8eed44b96da5eecc83889bf84e64
Cephas Tokoli <matumasie@gmail.com>
2024-01-25 22:10:14 +0000

    Update projectComponent.jsx

--- src/components/projectComponent.jsx
+++ src/components/projectComponent.jsx
@@ -16,20 +16,7 @@ import 'react-tooltip/dist/react-tooltip.css';
 // };
 
 const ProjectComponent = () => {
-    // const [modalIsOpen, setIsOpen] = useState(false)
-    // let subtitle;
-
-    // function openModal() {
-    //     setIsOpen(true);
-    // }
-
-    // function afterOpenModal() {
-    //     // references are now sync'd and can be accessed.
-    // }
-
-    // function closeModal() {
-    //     setIsOpen(false);
-    // }
+    
     return (
         <div className="exp md:p-24 ">
             <div className=" w-28 ">
@@ -37,31 +24,23 @@ const ProjectComponent = () => {
             </div>
             <div className="projects grid md:grid-cols-3 gap-8 md:gap-x-20 mt-14 m-auto justify-center">
                 {data.map((project, index) => (
-                    <div key={index} className=" w-80 text-slate-800 mb-5 bg-fuchsia-100 rounded-md" data-tooltip-id="project" data-tooltip-content={project.project_name} >
+                    <div key={index} className="w-80 text-slate-800 mb-5 bg-fuchsia-100 rounded-md animate__animated animate__zoomIn duration-500 delay-200" data-tooltip-id="project" data-tooltip-content={project.project_name} >
                         <img className="p-5" src={project.project_image} alt="project image" />
                        <div className="flex flex-col justify-around"> 
                        <div className="pl-5 pr-3">
-                            <h2 className=" font-bold">{project.project_name}</h2>
+                            <h2 className="font-bold">{project.project_name}</h2>
                             <p className="pt-2">{project.description}</p>
                         </div>
+                        <div className="pl-5 pr-3 mt-5">
+                            <h3 className="font-bold">Tech Stack</h3>
+                            <p>{project.tech_stack}</p>
+                        </div>
                         <div className="pl-5 mt-5 pb-5">
-                            <a href={project.link} className="bg-pink-500 px-6 py-2 text-slate-100 mr-5" >View</a>
-                            <a href={project.github} className=" bg-slate-500 px-6 py-2 text-slate-100 mr-5">Github</a>
+                            <a href={project.link} target="_blank" className="bg-pink-500 px-6 py-2 text-slate-100 mr-5" >View</a>
+                            <a href={project.github} target="_blank" className=" bg-slate-500 px-6 py-2 text-slate-100 mr-5">Github</a>
                         </div>
                        </div>
-                        {/* <Modal
-                            isOpen={modalIsOpen}
-                            onAfterOpen={afterOpenModal}
-                            onRequestClose={closeModal}
-                            style={customStyles}
-                            contentLabel="Example Modal"
-                        >
-                            <button onClick={closeModal}>close</button>
-                            <div>
-                                <img src={project.project_image} alt="" />
-                                <h2 className="">{project.project_name}</h2>
-                            </div>
-                        </Modal> */}
+                    
                     </div>
 
                 ))}
