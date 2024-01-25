2236cfeae3c983426ea629b5fecabaa09187c416
Cephas Tokoli <matumasie@gmail.com>
2024-01-25 22:10:14 +0000

    Update projectData.jsx

--- src/components/projectData.jsx
+++ src/components/projectData.jsx
@@ -1,44 +1,50 @@
 const data = [
     {
         "project_name": 'Dragula',
-        "description": 'Dragula is a React Single Page Application designed for file uploads, featuring both sign-in and sign-up functionality using Firebase Authentication.',
+        "description": 'Dragula Single Page Application is a React Application that allows drag-and-drop file uploads. It supports both sign-in and sign-up functionality through Firebase Authentication.',
         "project_image" : './images/dragula.png',
+        "tech_stack": "React, React Router, Firebase Auth, React-Dropzone",
         "link": "https://dragula.onrender.com",
         "github": "https://github.com/ctokoli/Dragula"
 
     },
     {
         "project_name": 'Spaceship',
-        "description": 'Spaceship: You will have access to rockets, missions and dragons even to reserve and participate through our react application.',
+        "description": 'Spaceship is a React application that allows users to reserve rides in rockets, missions, and dragons sections of the application. Users can also delete or remove reservations.',
         "project_image" : './images/spaceship.png',
+        "tech_stack": "React, React Router, Redux Toolkit, Jest",
         "link": "https://rocket-missions.netlify.app/",
         "github": "https://github.com/rubydevi/react-group-project",
     },
     {
         "project_name": 'Franko Trading Ecommerce Mobile app',
-        "description": 'Franko Trading Mobile App developed with #flutter and Woocommerce API with payment system, cart management, and an address management system.',
+        "description": 'Franko Trading\'s mobile app was developed using Woocommerce API and Flutter. It allows users to register, log in, and shop for electronics. The app features a payment system, cart management, and an address management system.',
         "project_image" : './images/mobile_app.jpg',
+        "tech_stack": "Dart, Flutter, Firebase, JWT, Hive",
         "link": "https://play.google.com/store/apps/details?id=com.frakotg.enterapp&hl=en&gl=US",
-        "github": ""
+        "github": "https://github.com/ctokoli/mobile-app"
     },
     {
         "project_name": 'Movie App',
-        "description": 'This project was created using some JavaScript features like webpack, ES6 features, and Jest testing framework. We consume tvmaze API and UI styling.',
+        "description": 'This project was developed using JavaScript features such as Webpack, ES6, and Jest for testing. We consume the tvmaze API and CSS for UI styling.',
         "project_image" : './images/movie_app_1.png',
+        "tech_stack": "Javascript, Webpack, Jest",
         "link": "https://moviehouse.onrender.com",
         "github": "https://github.com/ctokoli/JavaScript-Group-Capstone-Project"
     },
     {
         "project_name": 'Movie App',
-        "description": 'MovieBox is a React Single Page Application that shows top 10 rated movies from The Movie DB (TMDB) API.',
+        "description": 'MovieBox is a React Single Page Application that displays top-rated movies from the Movie DB (TMDB) API.',
         "project_image" : './images/movie_app_2.png',
+        "tech_stack": "React, React Router, React Query, React Player, Jest",
         "link": "https://moviebox-a18k.onrender.com",
         "github": "https://github.com/ctokoli/Movie-Box"
     },
     {
         "project_name": 'Calculator',
-        "description": 'This is a React project were i create a single page Application using React JS Technology. I create a Calculator App.',
+        "description": 'The Calculator App is a project created using React. It is designed to perform various calculations with a user-friendly interface',
         "project_image" : './images/math.png',
+        "tech_stack": "Javascript, React, React Router, ESlint, Jest",
         "link": "https://react-calculator-a58z.onrender.com",
         "github": "https://github.com/ctokoli/math-magicians-react-app"
     }
